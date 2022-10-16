e2fe79df0d3dbf7ba9c76f60c5f7e8ead177decf
IraBarbar <bro998877@yahoo.com>
2022-10-16 12:56:49 +0300

    Example023_Method_Array

---
+++ Example023_Method_Array/Program.cs
@@ -0,0 +1,17 @@
+﻿    int[] array = {1, 55, 44, 88, 64, 74, 22, 50, 95, 44};
+
+
+    int n = array.Length;
+    int find = 44;
+    int index = 0;
+
+    while ( index < n)
+    {
+        if(array[index]  == find)
+        {
+            Console.WriteLine(index);
+            break;
+    
+        }
+        index++;
+    }
