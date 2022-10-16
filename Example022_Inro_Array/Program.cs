863f3e9033f8cbb1ee4bd5879e55c9483039804d
IraBarbar <bro998877@yahoo.com>
2022-10-16 12:16:01 +0300

    Example022_Inro_Array

---
+++ Example022_Inro_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿    int Max(int arg1, int arg2, int arg3)
+                {
+                    int result = arg1;
+                    if (arg2 > result) result = arg2;
+                    if (arg3 > result) result = arg3;
+                    return result;
+                }
+                
+    int[] array = {11, 55, 44, 66, 88, 55, 99, 100, 87};
+    array[5] = 6; //заменили число 55 на 5
+
+        int max = Max(
+                Max(array[0], array[1], array[2]),
+                Max(array[3], array[4], array[5]),
+                Max(array[6], array[7], array[8])
+                );
+
+    Console.Write(max);
